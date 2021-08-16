4c6102610eaaf3a9da91dd9d511deaa426db52aa
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-15 19:02:26 -0700

    Wrote all the questions

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const fs = require("fs")
 const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the Title of your project?",
+    },
+    {
+        type: "input",
+        name: "projectDesc",
+        message: "Write a description for your project.",
+    },
+    {
+        type: "input",
+        name: "insallationInstruct",
+        message: "How do you install the project?",
+    },
+    {
+        type: "input",
+        name: "projectUsage",
+        message: "Write some Usage Information",
+    },
+    {
+        type: "input",
+        name: "ContributionInstruct",
+        message: "How can someone contribute to the project?",
+    },
+    {
+        type: "input",
+        name: "testInstruct",
+        message: "Write some test instructions",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license do you want?",
+        choices: ["Apache License 2.0", "GNU GPLv3", "GNU GPLv2", "MIT", "ISC License", "No License"]
+    },
+    {
+        type: "input",
+        name: "gitHubUser",
+        message: "What is your GitHub Username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
